fca3cc5c60770561922b78de061769adec9c4df1
Hayden-Haun <haydenhaun@gmail.com>
2021-10-04 10:21:24 -0600

    updated resolvers.js and server.js with authMiddleware code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,7 +12,7 @@ const resolvers = {
 
     me: async (parent, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate("thoughts");
+        return User.findOne({ _id: context.user._id }).populate("savedBooks");
       }
       throw new AuthenticationError("You need to be logged in!");
     },
