968355ea1a2b2164928c886ca3360e85e437f0f7
Hayden-Haun <haydenhaun@gmail.com>
2021-10-04 10:29:14 -0600

    added args in the me query of resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,7 +10,7 @@ const resolvers = {
     //   });
     // },
 
-    me: async (parent, context) => {
+    me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id }).populate("savedBooks");
       }
