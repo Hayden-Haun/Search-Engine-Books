5db072404000cd008704c2463ea1cf6f97daeb5e
Hayden-Haun <haydenhaun@gmail.com>
2021-10-03 22:37:18 -0600

    added REMOVE_BOOK mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,19 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($)
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
