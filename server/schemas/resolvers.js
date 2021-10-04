8c4792fb01a380c1bcdd63fcbd15124279ba6f4c
Hayden-Haun <haydenhaun@gmail.com>
2021-10-04 10:20:07 -0600

    added authMiddleware to auth.js, server.js and resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,10 +4,17 @@ const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    me: async (parent, { userId, username }) => {
-      return User.findOne({
-        $or: [{ _id: userId }, { username: username }],
-      });
+    // me: async (parent, { userId, username }) => {
+    //   return User.findOne({
+    //     $or: [{ _id: userId }, { username: username }],
+    //   });
+    // },
+
+    me: async (parent, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate("thoughts");
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 
