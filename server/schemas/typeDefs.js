e8257270422dd7ea25e25b867c1fedaa7de23938
Hayden-Haun <haydenhaun@gmail.com>
2021-10-05 22:15:27 -0600

    updated login and signup forms, but JS still doesnt recgonize token

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,7 +29,7 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    createUser(username: String!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
     saveBook(bookId: String!): User
     deleteBook(bookId: String!): User
